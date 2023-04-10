5925f564428bec5f7a997fb60c1789fa224040de
Saw-YanLinOo <yly80103@gmail.com>
2023-04-10 17:14:04 +0630

    feat: add my card

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const card = {
+  title:"Card 1",
+  description:"accusamus beatae ad facilis cum similique qui sunt",
+  image:"https://via.placeholder.com/150/92c952"
+}
+const cardTwo = {
+  title:"Card 2",
+  description:"accusamus beatae ad facilis cum similique qui sunt",
+  image:"https://via.placeholder.com/150/92c952"
+}
+function CardList() {
+  return (<main className='card-list'>
+    <Card card={card} />
+    <Card card={cardTwo} />
+  </main>);
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Card(props){
+  return (
+    <section className='card'>
+      <img src={props.card.image} alt='card-image'/>
+      <h1 style={{marginTop:"1rem"}} >{props.card.title}</h1>
+      <CardDescription desc= {props.card.description}/>
+    </section>
+  )
+}
+
+function CardDescription(props){
+  return <p style={{marginTop:"0.5rem"}}>{props.desc}</p>
+}
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(<CardList/>)
